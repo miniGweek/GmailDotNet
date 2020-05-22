f9435acc89ae08b2825112160d8cb71e920bb884
Rahul Sarkar <rahul.sarkar@bluestone.com.au>
2020-05-23 08:57:52 +1000

    Blank API controller didn't work. Removing project and adding a working version of API template (asp.net core templates). This works.

---
+++ GmailApi/Controllers/EmailSearchController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GmailApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmailSearchController : ControllerBase
+    {
+
+        private readonly ILogger<EmailSearchController> _logger;
+
+        public EmailSearchController(ILogger<EmailSearchController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            return 1;
+        }
+    }
+}
--- GmailApi/Program.cs
+++ GmailApi/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace GmailDotNet
+namespace GmailApi
 {
     public class Program
     {
